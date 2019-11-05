897720b6fdd75f6d60191aa3604ec85ecf1e0a75
alize <alizee.bouhana1@uqac.ca>
2019-11-05 09:50:50 -0500

    crawler done

--- src/Crawler.java
+++ src/Crawler.java
@@ -4,6 +4,7 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.json.*;
 
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.regex.*;
 
@@ -12,88 +13,57 @@ import java.util.HashSet;
 
 public class Crawler {
 
-    public static Spell crawlSpells(String link){
-        try {
-            Document doc = Jsoup.connect(link).get();
-            Elements DivSpellOffense = doc.select("div.statblock"); //div de toute les stats de la créature
-            DivSpellOffense = DivSpellOffense.select("p:contains(OFFENSE) + p"); //slection balise p suivant titre OFFENSE
+    public static Spell crawlSpells(String link) throws IOException {
 
-            System.out.println(DivSpellOffense);
-
-            ArrayList<String> spellsList = new ArrayList<>();
-            Elements spells = DivSpellOffense.select("a.spell");
-            for (Element spell : spells){
-                String url = spell.toString();
-                url = url.substring(url.indexOf("href")+6);
-                url = url.substring(0, url.indexOf("\""));
-                spellsList.add(url.toString());
-            }
-
-            for (String url : spellsList) {
-                System.out.println(url);
-            }
+        Document doc = Jsoup.connect(link).get();
+        Elements spellsPrepared = doc.select("p:contains(Spells Prepared) + p, p:contains(Spell-Like Abilities) + p"); //selection p suivant <p>Spells Prepared ou <p>Spells Ability
 
 
-
-
-
-
-
-
-            /*
-            Elements spdet = spell.select("p.SPDet"); // div contenant les infos component, level, spellResist
-
-            //name
-            String name = spell.select("div.heading p").toString().replaceAll("</*p>", "");
-
-            //components
-            ArrayList<String> components = new ArrayList<>();
-            String comp = spdet.select("p:matches(Components)").toString();
-            comp = comp.substring(comp.indexOf("<b>Components</b>")+17);
-            Pattern pattern = Pattern.compile("[A-Z]*(/?[A-Z]+)");
-            Matcher matcher = pattern.matcher(comp);
-            while(matcher.find()){
-                components.add(matcher.group());
-            }
-
-            //level
-            int level = 0;
-            String lev = spdet.select("p:matches(Level)").toString();
-            lev = lev.substring(lev.indexOf("<b>Level</b>")+12);
-            pattern = Pattern.compile("[0-9]+");
-            matcher = pattern.matcher(lev);
-            while(matcher.find()){
-                int i = Integer.parseInt(matcher.group());
-                if(i > level){
-                    level = i;
-                }
-            }
-
-            //spellResist
-            boolean spellResist;
-            String resist = spdet.select("p:contains(Spell Resistance yes)").toString();
-            spellResist = !resist.equals("");
-
-
-            // Spell
-            return new Spell(name, level, components, spellResist);
-*/
-
-        } catch (IOException e) {
-            System.err.println("For '" + link + "': " + e.getMessage());
+        ArrayList<String> spellsList = new ArrayList<>(); //TODO: doublons possibles !!!
+        Elements spells = spellsPrepared.select("a.spell");
+        for (Element spell : spells){
+            String url = spell.toString();
+            url = url.substring(url.indexOf("href")+6);
+            url = url.substring(0, url.indexOf("\""));
+            spellsList.add(url);
         }
 
+        System.out.print((!spellsList.isEmpty())?"creature " + link + "\n": "");
+
+        //return erreur (spell vide)
         return new Spell(null, 0, new ArrayList<>(), false);
     }
 
-    public static void main(String[] args) {
-        //TODO: récuperer liste des créatures du bestiaires avec links
-        //https://www.d20pfsrd.com/bestiary/bestiary-alphabetical/bestiary-a-b/
-        //https://www.d20pfsrd.com/bestiary/bestiary-alphabetical/bestiary-c-d/
-        // ...
+    public static void main(String[] args) throws IOException {
+        Document docHome = Jsoup.connect("https://www.d20pfsrd.com/bestiary/Monster-listings").get();
+        Elements urlOutsider = docHome.select("td:contains(Outsiders) li.page.new.parent a");
+        for(Element outsider : urlOutsider){
+            String url = outsider.toString();
+            url = url.substring(url.indexOf("\"")+1);
+            url = url.substring(0, url.indexOf("\""));
 
+            Document doc = Jsoup.connect(url).get();
+            Elements typeUrl = doc.select("h4:contains(Binding)");
+
+            if(!typeUrl.isEmpty()){
+                Elements binds = doc.select("div.page-center div.ogn-childpages a[href~=.*bestiary/monster-listings/outsiders/.*]");
+                for(Element bind : binds) {
+                    url = binds.toString();
+                    url = url.substring(url.indexOf("\"") + 1);
+                    url = url.substring(0, url.indexOf("\""));
+                }
+            }
+
+            Spell s = crawlSpells(url); //envoi au crawler
+        }
+
+
+
+/*
         String link = "https://www.d20pfsrd.com/bestiary/monster-listings/outsiders/azata/bralani/";
         Spell s = crawlSpells(link);
+        */
+
 /*
         ArrayList<Spell> arraySpells = new ArrayList<>();
         // boucle pour tous les spells
